2a30a411029bc4443b9600444d60e0e5a26cb108
Joseph Hassett <jchassett818@gmail.com>
2019-11-09 17:20:44 -0600

    Added some card updates and image upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, :dependent => :delete_all
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  mount_uploader :photo, PhotoUploader
 end
